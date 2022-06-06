765958ad8ddca233595021486052f005882d1147
Landy510 <randy.chen@tpisoftware.com>
2022-06-06 18:17:42 +0800

    feat: 首頁 1. 加入 tailwind 的切版功能

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/app/*.{html,ts}"],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#769763',
+      'grey': '#737373'
+    },
+  },
+  plugins: [],
+}
