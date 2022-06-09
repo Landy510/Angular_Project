4d00005c6136818b7d173c8e4b8380de16030bf4
Landy510 <randy.chen@tpisoftware.com>
2022-06-09 12:14:53 +0800

    feat: 調整元件結構，加入 router 的功能

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './Views/Home/home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
