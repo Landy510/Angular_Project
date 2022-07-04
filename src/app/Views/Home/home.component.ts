b4d496606ec0fe32cbd2d006a9dc756b6c927a7f
Landy510 <randy.chen@tpisoftware.com>
2022-07-04 13:17:48 +0800

    feat: 首頁 1. 新增 - banner 中的搜尋框功能

--- src/app/Views/Home/home.component.ts
+++ src/app/Views/Home/home.component.ts
@@ -11,6 +11,42 @@ import { HomeService } from '@api/home/home.service';
 export class HomeComponent implements OnInit {
   @ViewChild(ElementRef) dropDownCompo!: ElementRef;
 
+  typeList: string[] = [
+    '旅遊景點',
+    '觀光活動',
+    '美食品嚐',
+    '住宿推薦',
+  ];
+
+  cityList: string[] = [
+    '基隆市',
+    '台北市',
+    '新北市',
+    '桃園縣',
+    '新竹市',
+    '新竹縣',
+    '苗栗縣',
+    '台中市',
+    '彰化縣',
+    '南投縣',
+    '雲林縣',
+    '嘉義市',
+    '嘉義縣',
+    '台南市',
+    '高雄市',
+    '屏東縣',
+    '台東縣',
+    '花蓮縣',
+    '宜蘭縣',
+    '澎湖縣',
+    '金門縣',
+    '連江縣',
+  ];
+
+  typeOption: string = '';
+
+  cityOption: string = '';
+
   constructor(
     private homeService: HomeService,
     private matIconRegistry: MatIconRegistry,
@@ -33,5 +69,10 @@ export class HomeComponent implements OnInit {
 
   toggle(el: HTMLDivElement): void {
     el.classList.contains('hidden') ?  el.classList.remove('hidden') : el.classList.add('hidden');
+  }
+
+  setSearchContent(el: HTMLDivElement, option: string, type: string): void {
+    el.classList.contains('hidden') ?  el.classList.remove('hidden') : el.classList.add('hidden');
+    type === 'type' ? this.typeOption = option : this.cityOption = option;
   }
 }
