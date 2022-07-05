1e38efe4b7f87ab81332b3afe44a6d15995a2196
Landy510 <randy.chen@tpisoftware.com>
2022-07-05 18:38:01 +0800

    feat: header 元件 1. 新增手機板的切版 2. 新增背景圖的效果

--- src/app/Views/header/header.component.ts
+++ src/app/Views/header/header.component.ts
@@ -1,33 +1,41 @@
 import {
   Component,
   OnInit,
-  OnChanges,
-  Output,
-  EventEmitter,
-  Input,
-  SimpleChanges } from '@angular/core';
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit, OnChanges {
-  @Input() sideMenuOpen !: boolean;
+export class HeaderComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChild('headerRef')headerRef!: ElementRef;
 
-  @Output() toggle = new EventEmitter<null>();
+  scrollEvt!: any;
 
   constructor() {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes['sideMenuOpen']);
-  }
-
   ngOnInit(): void {}
 
-  onToggle(): void {
-    console.log('123123');
-    this.toggle.emit(null);
+  onToggle(el: HTMLSpanElement): void {
+    el.classList.toggle('hidden');
+  }
+
+  ngAfterViewInit(): void {
+    this.scrollEvt = () => {
+      window.pageYOffset > 520 ? // 當滑超過 bannerImage 的高度時，為 header 加入背景圖
+        this.headerRef.nativeElement.classList.add('over')
+        : this.headerRef.nativeElement.classList.remove('over');
+    };
+    window.addEventListener('scroll', this.scrollEvt, { passive: true });
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.scrollEvt);
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { MatIconRegistry } from '@angular/material/icon';
-// import { DomSanitizer } from '@angular/platform-browser';
-
-// import { TranslateService } from '@ngx-translate/core';
 
 import { AuthService } from '@api/auth/auth.service';
 @Component({
@@ -11,45 +7,8 @@ import { AuthService } from '@api/auth/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'practice';
 
-  showFiller = false;
-
-  links = [
-    '連結一',
-    '連結二',
-  ];
-
-  constructor(
-    // private matIconRegistry: MatIconRegistry,
-    // private domSanitizer: DomSanitizer,
-    private authService: AuthService
-    // public translate: TranslateService
-  ) {
-    // this.translate.addLangs(['zh', 'en', 'ja']);
-    // this.translate.use('zh');
-    // this.matIconRegistry.addSvgIconInNamespace(
-    //   'custom-svg',
-    //   'angular',
-    //   this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/angular_solidBlack.svg')
-    // );
-    // this.matIconRegistry.addSvgIconInNamespace(
-    //   'custom-svg',
-    //   'facebook',
-    //   this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/footer_facebook.svg')
-    // );
-    // this.matIconRegistry.addSvgIconInNamespace(
-    //   'custom-svg',
-    //   'instagram',
-    //   this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/footer_instagram.svg')
-    // );
-    // this.matIconRegistry.addSvgIconInNamespace(
-    //   'custom-svg',
-    //   'youtube',
-    //   this.domSanitizer.bypassSecurityTrustResourceUrl('assets/images/footer_youtube.svg')
-    // );
-    // this.matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
-  }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.postToGetToken()
@@ -60,8 +19,4 @@ export class AppComponent implements OnInit {
         error: (err) => console.log('token acquire fail', err)
       });
   }
-
-  // switchLang(lang: string) {
-  //   this.translate.use(lang);
-  // }
 }
